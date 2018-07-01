72e940ebea7d038284cec5a8c7edfd0067ffe55c
onias85@gmail.com <onias85@gmail.com>
2018-07-01 09:05:26 -0300

    adicionando biblioteca do Jama

--- opt_java/src/main/java/library_example/matrix_jama.java
+++ opt_java/src/main/java/library_example/matrix_jama.java
@@ -3,16 +3,13 @@ import Jama.Matrix;
 
 public class matrix_jama {
 
-	public class main {
 
-		public static void main(String[] args) {
-			double[][] array = {{1.,2.,3},{1.,2.,3.},{1.,2.,3.}}; 
-			Matrix a = new Matrix(array);   
-			Matrix b = new Matrix(new double[]{1., 1., 1.}, 1);     
-			Matrix c = b.times(a);  
-			//System.out.println(Arrays.deepToString(c.getArray()));
-
-		}
+	public static void main(String[] args) {
+		double[][] array = {{1.,2.,3},{1.,2.,3.},{1.,2.,3.}}; 
+		Matrix a = new Matrix(array);   
+		Matrix b = new Matrix(new double[]{1., 1., 1.}, 1);     
+		Matrix c = b.times(a);  
+		//System.out.println(Arrays.deepToString(c.getArray()));
 
 	}
 }
