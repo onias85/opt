941c86d4ff984be6317e59c94ea9e7e4060305b9
oliveirampo <marynna.oliveira@hotmail.com>
2018-07-01 15:23:00 +0200

    matrix

--- opt_java/src/main/java/library_example/matrix_jama.java
+++ opt_java/src/main/java/library_example/matrix_jama.java
@@ -7,15 +7,18 @@ public class matrix_jama {
 
 
 	public static void main(String[] args) {
-		double[][] array = {{1.,2.,3},{1.,2.,3.},{1.,2.,3.}}; 
-		Matrix a = new Matrix(array);   
-		Matrix b = new Matrix(new double[]{1., 1., 1.}, 1);     
-		Matrix c = b.times(a);  
-		System.out.println(Arrays.deepToString(c.getArray()));
+		//double[][] array = {{1.,2.,3},{1.,2.,3.},{1.,2.,3.}}; 
+		//Matrix a = new Matrix(array);   
+		//Matrix b = new Matrix(new double[]{1., 1., 1.}, 1);     
+		//Matrix c = b.times(a);  
+		//System.out.println(Arrays.deepToString(a.getArray()));
 		
+		double[][] array = {{1.,2.,3},{4.,5.,6.},{7.,8.,10.}};
 		Matrix A = new Matrix(array);
 		Matrix b = Matrix.random(3,1);
 		Matrix x = A.solve(b);
-		System.out.println(Arrays.deepToString(c.getArray()));
+		System.out.println(Arrays.deepToString(A.getArray()));
+		System.out.println(Arrays.deepToString(b.getArray()));
+		System.out.println(Arrays.deepToString(x.getArray()));
 	}
 }
