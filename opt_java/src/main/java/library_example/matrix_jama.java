b186f78ce6a9d6f001f7efc483d1209deac32a24
oliveirampo <marynna.oliveira@hotmail.com>
2018-07-01 15:35:06 +0200

    matrix

--- opt_java/src/main/java/library_example/matrix_jama.java
+++ opt_java/src/main/java/library_example/matrix_jama.java
@@ -13,9 +13,11 @@ public class matrix_jama {
 		//Matrix c = b.times(a);  
 		//System.out.println(Arrays.deepToString(a.getArray()));
 		
-		double[][] array = {{1.,2.,3},{4.,5.,6.},{7.,8.,10.}};
+		double[][] array = {{1.,1.,1},{0.,2.,5.},{2.,5.,-1.}};
 		Matrix A = new Matrix(array);
-		Matrix b = Matrix.random(3,1);
+		//Matrix b = Matrix.random(3,1);
+		double[][] array2 = {{6.},{-4.},{27.}}; 
+		Matrix b = new Matrix(array2);
 		Matrix x = A.solve(b);
 		System.out.println(Arrays.deepToString(A.getArray()));
 		System.out.println(Arrays.deepToString(b.getArray()));
