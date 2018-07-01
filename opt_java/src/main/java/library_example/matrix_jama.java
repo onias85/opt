9ddd11b10b58158a1e9c98962d2e71752355c424
oliveirampo <marynna.oliveira@hotmail.com>
2018-07-01 13:54:56 +0200

    Jama library

---
+++ opt_java/src/main/java/library_example/matrix_jama.java
@@ -0,0 +1,18 @@
+package library_example;
+import Jama.Matrix;
+
+public class matrix_jama {
+
+	public class main {
+
+		public static void main(String[] args) {
+			double[][] array = {{1.,2.,3},{1.,2.,3.},{1.,2.,3.}}; 
+			Matrix a = new Matrix(array);   
+			Matrix b = new Matrix(new double[]{1., 1., 1.}, 1);     
+			Matrix c = b.times(a);  
+			//System.out.println(Arrays.deepToString(c.getArray()));
+
+		}
+
+	}
+}
